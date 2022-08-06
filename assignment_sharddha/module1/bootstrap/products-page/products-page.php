8dfe31b9dcb40c23b98ac35615acc4e4c63918d6
Brijesh1990 <bkpandey.pandey@gmail.com>
2022-08-06 06:23:38 +0200

    Add files via upload

---
+++ assignment_sharddha/module1/bootstrap/products-page/products-page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link  rel="stylesheet" href="css/style.css">
+    <link  rel="stylesheet" href="css/bootstrap.min.css">
+    <link  rel="stylesheet" href="css/bootstrap-grid.css">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/bootstrap.js.map"></script>
+</head>
+<body>
+    <div class="container bg-white p-5 mt-5 shadow-lg">
+        <div class="row">
+            <div class="col-md-6">
+             
+            </div>
+            
+          
+            
+            <div class="col-md-6">
+            
+             </div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
---
+++ assignment_sharddha/module1/bootstrap/profile-page/manage-profile.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link  rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+    <div class="container bg-white p-5 mt-5 shadow-lg">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="images/sharddha.png" class="rounded-circle w-75 ml-5 mx-auto" style="margin-left: 10% !important">
+                <br>
+                <p class="text-center">Sharddha</p>
+                <p class="text-center">Age : 22 years lod</p>
+            </div>
+            
+            <div class="col-md-5 shadow">
+                <h4 class="p-4">Profile Setting</h4>
+            </div>
+            
+            <div class="col-md-4 shadow">
+            <h5 class="p-4">Experience</h5>
+        </div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
